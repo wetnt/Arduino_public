2ab7929e6031da7d571f5adece570049e66208e4
jinping.xu <jinping.xu@woqu.com>
2016-04-13 09:06:32 +0800

    G5 _LOOP_OK

---
+++ PlanTower/G5_HOHC_Wifi_Lewei_1/G5_HCHO_Wifi_Lewei_6_5110_ok/B5_WiFi.ino
@@ -0,0 +1,191 @@
+//=========================================================================
+//https://www.zybuluo.com/kfihihc/note/31135
+//http://wiki.iteadstudio.com/ESP8266_Serial_WIFI_Module
+//=========================================================================
+const static char AT_Test[] PROGMEM = "AT";
+const static char AT_Type[] PROGMEM = "AT+CWMODE=1"; //beWiFi
+const static char AT_Rest[] PROGMEM = "AT+RST";       //reBoot
+const static char AT_Cmux[] PROGMEM = "AT+CIPMUX=0";  //?
+const static char AT_IsJP[] PROGMEM = "AT+CWJAP?";    //SeeAP//is Join AP
+const static char AT_lgIP[] PROGMEM = "AT+CIFSR";     //seesIP
+const static char AT_JAPa[] PROGMEM = "AT+CWJAP=\"";  //Join AP
+const static char AT_JAPb[] PROGMEM = "\",\"";        //
+const static char AT_JAPc[] PROGMEM =  "\"";          //
+const static char AT_Send[] PROGMEM = "AT+CIPSEND=";  //
+const static char AT_Stat[] PROGMEM = ">";            //
+//-------------------------------------------------------------------------
+const static char AT_OKss[] PROGMEM = "OK";
+const static char AT_Busy[] PROGMEM = "busy";
+const static char AT_Errs[] PROGMEM = "ERROR";
+const static char AT_RstB[] PROGMEM = "Ai-Thinker";
+const static char AT_Fail[] PROGMEM = "FAIL";
+const static char AT_Cnet[] PROGMEM = "CONNECT";
+const static char AT_Clsd[] PROGMEM = "CLOSED";
+const static char AT_Empt[] PROGMEM = "";
+//=========================================================================
+String wifiAP = "", wifiIP = "";
+//=========================================================================
+//#include <SoftwareSerial.h>
+//SoftwareSerial wfs(10, 11); // RX, TX
+#define wfs Serial2
+//=========================================================================
+static const long wifiBaud = 115200;//9600;
+boolean wifiRun = false, wifiLog = false;
+String wifiBack = "";
+//=========================================================================
+void wifi_Init(boolean lgKey, boolean startAP, boolean joinAP) {
+  //-------------------------------------------------------------------------
+  wfs.begin(wifiBaud); lg(F("WiFi.Serial.start! = ")); lg(wifiBaud); lg();
+  //-------------------------------------------------------------------------
+  wifiLog = lgKey;
+  //-------------------------------------------------------------------------
+  if (startAP) netStartAP();
+  if (joinAP) //netJoinAP(F("MMMM"), F("1234567890123"));
+    netJoinAP(F("acDev"), F("AbroadCar2015()"));
+  lgln(wifiAP); lgln(wifiIP);
+  //-------------------------------------------------------------------------
+}
+//=========================================================================
+void netStartAP() {
+  //-------------------------------------------------------
+  wifiRun = wfs_cmd_back(S(AT_Test), S(AT_OKss), S(AT_Empt), 1000); //test
+  wifiRun = wfs_cmd_back(S(AT_Cmux), S(AT_OKss), S(AT_Empt), 1000); //be 1 LINK MODE
+  wifiRun = wfs_cmd_back(S(AT_IsJP), S(AT_OKss), S(AT_Empt), 1000); lgln(wifiBack); wifiAP = GetStringAB(wifiBack, F("\""), F("\"\r\n")); //SeeAP
+  wifiRun = wfs_cmd_back(S(AT_lgIP), S(AT_OKss), S(AT_Empt), 1000); lgln(wifiBack); wifiIP = GetStringAB(wifiBack, F("\""), F("\"\r\n")); //seesIP
+  //lgln(wifiAP); lgln(wifiIP);
+  //-------------------------------------------------------
+}
+void netJoinAP(String n, String p) {
+  //-------------------------------------------------------------------------
+  String AT_Join = S(AT_JAPa) + n + S(AT_JAPb) + p + S(AT_JAPc);
+  //-------------------------------------------------------------------------
+  wifiRun = wfs_cmd_back(S(AT_Test), S(AT_OKss), S(AT_Empt), 1000 ); //test
+  wifiRun = wfs_cmd_back(S(AT_Type), S(AT_OKss), S(AT_Empt), 1000 ); //beWiFi
+  wifiRun = wfs_cmd_back(S(AT_Rest), S(AT_RstB), S(AT_Empt), 5000 ); //reBoot
+  wifiRun = wfs_cmd_back(S(AT_Cmux), S(AT_OKss), S(AT_Empt), 1000 ); //lgln(s);//?
+  wifiRun = wfs_cmd_back(  AT_Join , S(AT_OKss), S(AT_Empt), 10000); //lgln(s);//joinAP
+  wifiRun = wfs_cmd_back(S(AT_IsJP), S(AT_OKss), S(AT_Fail), 1000 ); wifiAP = GetStringAB(wifiBack, F("\""), F("\"\r\n")); //SeeAP
+  wifiRun = wfs_cmd_back(S(AT_lgIP), S(AT_OKss), S(AT_Empt), 1000 ); wifiIP = GetStringAB(wifiBack, F("\""), F("\"\r\n")); //seesIP
+  //lgln(wifiAP); lgln(wifiIP); //for (;;);
+  //-------------------------------------------------------------------------
+}
+//-------------------------------------------------------------------------
+String senx = "";
+void HttpGetPost(String link, String text, int seconds) {
+  senx = S(AT_Send) + String(text.length() + String(text.length()).length() ); //lgln(senx);
+  wifiRun = wfs_cmd_back(link, S(AT_Cnet), S(AT_Errs), 2000 ); //lg(wifiBack); lg('='); lg(wifiRun); lg();
+  wifiRun = wfs_cmd_back(senx, S(AT_Stat), S(AT_Errs), 2000 ); //lg(wifiBack); lg('='); lg(wifiRun); lg();//if (!wifiRun) return;
+  wifiRun = wfs_cmd_back(text, S(AT_Clsd), S(AT_Empt), 20000); //lg(wifiBack); lg('='); lg(wifiRun); lg(); //if (!wifiRun) return;
+}
+//=========================================================================
+const static char Link_Baidu[] PROGMEM = {"AT+CIPSTART=\"TCP\",\"www.baidu.com\",80"};
+const static char Urls_Baidu[] PROGMEM = {"GET / HTTP/1.0\r\n\r\n"};
+void NetLinkTest_Baidu() {
+  HttpGetPost(S(Link_Baidu), S(Urls_Baidu) , 5); lgln(wifiBack);
+}
+const static char Link_BkGps[] PROGMEM = {"AT+CIPSTART=\"TCP\",\"www.bbkgps.com\",80"};
+const static char Urls_BkGps[] PROGMEM = {"GET http://www.bbkgps.com/t.php HTTP/1.0\r\n\r\n"};
+void NetLinkTest_BBKGPS() {
+  HttpGetPost(S(Link_BkGps), S(Urls_BkGps) , 10); lgln(wifiBack);
+}
+//=========================================================================
+//=========================================================================
+typedef struct charCheckSave {
+  int len = 0, inx = 0;
+  String str = "";
+  void init(String s) {
+    str = String(s); inx = 0;
+    len = s.length();
+    //lg("check=");lg(len); lg();
+  }
+  boolean check(char c) {
+    //lg("="); lg(c); lg("="); lg(inx); lg();
+    if (len == 0) return false;
+    if (inx == len - 1) {
+      inx = 0; return true;
+    }
+    if (str[inx] == c) inx++; else inx = 0;
+    return false;
+  }
+} charCheckSave;
+//=========================================================================
+charCheckSave ck1, ck2, ck3, ck4, ck5;
+//=========================================================================
+void wfs_serial_clear() {
+  while (wfs.available() > 0) {
+    char c = wfs.read();
+    if (wifiLog) lg(c);
+  }
+}
+boolean wfs_cmd_send(String comStr) {
+  wfs_serial_clear(); wfs.print(comStr); //lg(comStr);
+  smartDelay(1);
+  return true;
+}
+boolean wfs_cmd_backAB(String comStr, String strOK, String strNO, long timeout) { //
+  //-------------------------------------------------------
+  boolean save = false; char c; unsigned long start = millis(); wifiBack = "";
+  ck1.init(strOK);
+  ck2.init(strNO);
+  ck3.init(S(AT_Errs));
+  ck4.init(S(AT_Busy));
+  ck5.init(S(AT_Clsd));
+  //------------------------------------------------------
+  wfs_serial_clear(); wfs.println(comStr);
+  //-------------------------------------------------------
+  while (millis() - start < timeout) {
+    //-------------------------------------------------------
+    while (wfs.available() > 0) {
+      //-----------------------------------
+      c = wfs.read();
+      if (c == '\0') continue;
+      if (wifiLog) lg(c);
+      //-----------------------------------
+      if (save) wifiBack += c;
+      if (ck1.check(c)) save = true;
+      if (ck2.check(c)) save = false;
+      //-----------------------------------
+      //if (ck3.check(c)) return true;
+      //if (ck4.check(c)) return false;
+      if (ck5.check(c)) return false;
+      //-----------------------------------
+    }
+    //-------------------------------------------------------
+  }
+  //-------------------------------------------------------
+  return false;
+  //-------------------------------------------------------
+}
+boolean wfs_cmd_back(String comStr, String strOK, String strNO, long timeout) { //
+  //-------------------------------------------------------
+  char c; unsigned long start = millis(); wifiBack = "";
+  ck1.init(strOK);
+  ck2.init(strNO);
+  ck3.init(S(AT_Errs));
+  ck4.init(S(AT_Busy));
+  //------------------------------------------------------
+  wfs_serial_clear(); wfs.println(comStr);
+  //-------------------------------------------------------
+  while (millis() - start < timeout) {
+    //-------------------------------------------------------
+    while (wfs.available() > 0) {
+      //-----------------------------------
+      c = wfs.read();
+      if (c == '\0') continue;
+      if (wifiLog) lg(c);
+      //-----------------------------------
+      wifiBack += c;
+      //-----------------------------------
+      if (ck1.check(c)) return true;
+      if (ck2.check(c)) return false;
+      if (ck3.check(c)) return false;
+      if (ck4.check(c)) return false;
+      //-----------------------------------
+    }
+    //-------------------------------------------------------
+  }
+  //-------------------------------------------------------
+  return false;
+  //-------------------------------------------------------
+}
+//=========================================================================
---
+++ PlanTower/G5_HOHC_Wifi_Lewei_1/G5_HCHO_Wifi_Lewei_6_5110_ok/G5_HCHO_Wifi_Lewei_6_5110_ok.ino
@@ -0,0 +1,32 @@
+static const String codeName = "G5_HOHC_Wifi_Lewei_1!";
+void setup() {
+  //------------------------------------------------
+  lgsetup(codeName);
+  LCD5110_setup();
+  //------------------------------------------------
+  Si7021_setup();
+  //------------------------------------------------
+  S1_init(F("G5"), 9600, false);
+  //S2_init(F("WIFI"), 9600, true);
+  S3_init(F("HCHO"), 9600, false);
+  //------------------------------------------------
+  wifi_Init(false, false, false); NetLinkTest_BBKGPS();
+  //------------------------------------------------
+  tLoop_Setup();
+  //------------------------------------------------
+  HCHO_Ask(); Si7021_read();
+  LCD_Show_All(); LG_Show_All();//Lewei_Loop();
+  //------------------------------------------------
+}
+void loop() {
+  lgFeed(); S1_feed(); S2_feed(); S3_feed();
+  Work_Loop();
+}
+static void smartDelay(unsigned long ms) {
+  unsigned long start = millis(); do {
+    //------------------------------------------
+    lgFeed(); S1_feed(); S2_feed(); S3_feed();
+    //------------------------------------------
+  } while (millis() - start < ms);
+}
+//=====================================================
